1d589320aca3393c96bd8fed2ce5a2bf5ae60d92
A-conanclub <jenson_shan@yeah.net>
2024-12-01 18:24:51 +0800

    Upload file

---
+++ Python Crash Course/2.3.4-delete_whitespace-20241201.py
@@ -0,0 +1,12 @@
+favorite_language = "python "
+print(favorite_language)
+
+print(favorite_language.rstrip())
+
+favorite_language = favorite_language.rstrip()
+print(favorite_language)
+
+favorite_language = " python "
+print(favorite_language.rstrip())
+print(favorite_language.lstrip())
+print(favorite_language.split())
