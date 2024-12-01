f2c094455867122116f5965627c77981c14db360
A-conanclub <jenson_shan@yeah.net>
2024-12-01 18:28:28 +0800

    Upload file

---
+++ Python Crash Course/2.3.5-remove_prefix-20241201.py
@@ -0,0 +1,7 @@
+nostarch_url = "https://nostarch.com"
+print(nostarch_url)
+
+print(nostarch_url.removeprefix("https://"))
+
+nostarch_url = nostarch_url.removeprefix("https://")
+print(nostarch_url)
