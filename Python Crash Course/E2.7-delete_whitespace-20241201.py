740454531a2701f2c21b4beba2592df3c05fa706
A-conanclub <jenson_shan@yeah.net>
2024-12-01 18:57:44 +0800

    Upload Exercise files

---
+++ Python Crash Course/E2.7-delete_whitespace-20241201.py
@@ -0,0 +1,6 @@
+name = "\tjason shane\n"
+print(name.title())
+
+print(name.lstrip().title())
+print(name.rstrip().title())
+print(name.strip().title())
---
+++ Python Crash Course/E2.8-file_extension-20241201.py
@@ -0,0 +1,4 @@
+filename = "python_notes.txt"
+print(filename)
+
+print(filename.removesuffix(".txt"))
