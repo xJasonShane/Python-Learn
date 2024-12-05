9277be56e3047dbf3af980e0cf402377b78e7718
A-conanclub <jenson_shan@yeah.net>
2024-12-05 15:39:50 +0800

    Update 3.1.1, 3.1.2 and 3.1.3 content

--- Python Crash Course/C3 List introduction/3.1-bicycles-20241205.py
+++ Python Crash Course/C3 List introduction/3.1-bicycles-20241205.py
@@ -1,2 +1,15 @@
 bicycles = ['trek', 'cannondale', 'redline', 'specialized']
 print(bicycles)
+
+# 3.1.1
+print(bicycles[0])  # 返回列表第一个元素
+print(bicycles[0].title())
+
+# 3.1.2
+print(bicycles[1])
+print(bicycles[3])
+print(bicycles[-1])  # 返回列表最后一个元素，-2代表倒数第二个，以此类推
+
+# 3.1.3
+message = f"My first bicycle was a {bicycles[0].title()}"
+print(message)
