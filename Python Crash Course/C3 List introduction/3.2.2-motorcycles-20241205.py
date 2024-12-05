713de1aadf73ae74677c8acd54d6e02dc0ca283d
A-conanclub <jenson_shan@yeah.net>
2024-12-05 16:09:05 +0800

    Upload file

---
+++ Python Crash Course/C3 List introduction/3.2.2-motorcycles-20241205.py
@@ -0,0 +1,19 @@
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+# 尾端添加元素
+motorcycles.append('ducati')
+print(motorcycles)
+
+# 创建空列表，依次尾端添加元素
+motorcycles = []
+motorcycles.append('honda')
+motorcycles.append('yamaha')
+motorcycles.append('suzuki')
+print(motorcycles)
+
+# 插入新元素
+motorcycles = ['honda', 'yamaha', 'suzuki']
+
+motorcycles.insert(0, 'ducati')
+print(motorcycles)
