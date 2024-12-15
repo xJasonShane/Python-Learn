8920ba5b81e84c2683859a1f93156b8e6e86e4ca
A-conanclub <jenson_shan@yeah.net>
2024-12-15 19:26:14 +0800

    Update file

---
+++ Python Crash Course/C4 Operation list/4.3.2-squares-20241215.py
@@ -0,0 +1,12 @@
+# squares = []
+# for value in range(1, 11):
+#     square = value ** 2
+#     squares.append(square)
+
+# print(squares)
+
+# 简便方法
+squares = []
+for value in range(1, 11):
+    squares.append(value ** 2)
+print(squares)
