c100ed8446ee37e3b32d82416ffc7d943d82f1cd
A-conanclub <jenson_shan@yeah.net>
2024-12-15 19:11:32 +0800

    Upload Exercise files

---
+++ Python Crash Course/C4 Operation list/E4.1-pizza-20241215.py
@@ -0,0 +1,4 @@
+pizzas = ['Margherita', 'Calzones', 'Marinara', 'Pepperoni', 'Capricciosa']
+for pizza in pizzas:
+    print(f"I like {pizza} pizza.")
+print("I really love pizza!")
---
+++ Python Crash Course/C4 Operation list/E4.2-animal-20241215.py
@@ -0,0 +1,4 @@
+animals = ['cat', 'dog', 'bird']
+for animal in animals:
+    print(f"A {animal} would make a great pet.")
+print("Any of these animals would make a great pet!")
